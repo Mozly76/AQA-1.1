3676a2d5539f1742c8b95d36aea824c79f41ff32
Sergey Kiselev <mozly76@yandex.ru>
2022-08-31 22:53:48 +0300

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    CashbackHackService cashbackHackService = new CashbackHackService();
+
+    @Test
+    void shouldReturnTheMissingAmountForBonus() {
+        int sum = 1_500;
+
+        int expected = 500;
+        int actual = cashbackHackService.remain(sum);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnTheMissingAmountForBonusIfTheActualAmountLessThanOneThousand() {
+        int sum = 500;
+
+        int expected = 500;
+        int actual = cashbackHackService.remain(sum);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldReturnTheMissingAmountForBonusIfTheActualAmountEqualsTheExpected() {
+        int sum = 1_000;
+
+        int expected = 00;
+        int actual = cashbackHackService.remain(sum);
+
+        assertEquals(expected, actual);
+    }
+}
