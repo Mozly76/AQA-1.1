21e56509b7e37a15911cd2d252a906c6f1490ebe
Sergey Kiselev <mozly76@yandex.ru>
2022-09-04 20:37:59 +0300

    Change tests for JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,15 +1,15 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     CashbackHackService cashbackHackService = new CashbackHackService();
 
     @Test
-    void shouldReturnTheMissingAmountForBonus() {
+    public void shouldReturnTheMissingAmountForBonus() {
         int sum = 1_500;
 
         int expected = 500;
@@ -19,7 +19,7 @@ class CashbackHackServiceTest {
     }
 
     @Test
-    void shouldReturnTheMissingAmountForBonusIfTheActualAmountLessThanOneThousand() {
+    public void shouldReturnTheMissingAmountForBonusIfTheActualAmountLessThanOneThousand() {
         int sum = 500;
 
         int expected = 500;
@@ -29,13 +29,13 @@ class CashbackHackServiceTest {
 
     }
 
-//    @Test
-//    void shouldReturnTheMissingAmountForBonusIfTheActualAmountEqualsTheExpected() {
-//        int sum = 1_000;
-//
-//        int expected = 00;
-//        int actual = cashbackHackService.remain(sum);
-//
-//        assertEquals(expected, actual);
-//    }
+    @Test
+    public void shouldReturnTheMissingAmountForBonusIfTheActualAmountEqualsTheExpected() {
+        int sum = 1_000;
+
+        int expected = 0;
+        int actual = cashbackHackService.remain(sum);
+
+        assertEquals(expected, actual);
+    }
 }
